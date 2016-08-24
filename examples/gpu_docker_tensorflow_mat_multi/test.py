c2e78b90b18b11a291ac0dcd558edbe2a30d3051
Qingsong Liu <liuqingsong@unisound.com>
2016-08-24 14:55:08 +0800

    Multiplication operation in GPU

--- examples/gpu_docker_tensorflow_mat_multi/test.py
+++ examples/gpu_docker_tensorflow_mat_multi/test.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 with tf.device('/cpu:0'):
     a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2,3], name='a')
     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3,2], name='b')
-c = tf.matmul(a,b)
+    c = tf.matmul(a,b)
 sess = tf.Session()
 print sess.run(c)
 
@@ -10,6 +10,6 @@ import tensorflow as tf
 with tf.device('/gpu:0'):
     a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2,3], name='a')
     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3,2], name='b')
-c = tf.matmul(a,b)
+    c = tf.matmul(a,b)
 sess = tf.Session()
 print sess.run(c)
