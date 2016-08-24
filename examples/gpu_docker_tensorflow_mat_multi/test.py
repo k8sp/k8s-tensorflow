c23614fe615b87b45dae8323c765f14676742f12
Qingsong Liu <liuqingsong@unisound.com>
2016-08-24 18:26:47 +0800

    More python style

--- examples/gpu_docker_tensorflow_mat_multi/test.py
+++ examples/gpu_docker_tensorflow_mat_multi/test.py
@@ -1,15 +1,15 @@
 import tensorflow as tf
-with tf.device('/cpu:0'):
-    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2,3], name='a')
-    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3,2], name='b')
-    c = tf.matmul(a,b)
-sess = tf.Session()
-print sess.run(c)
 
-import tensorflow as tf
-with tf.device('/gpu:0'):
+with tf.Session() as sess:
+  with tf.device('/cpu:0'):
     a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2,3], name='a')
     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3,2], name='b')
     c = tf.matmul(a,b)
-sess = tf.Session()
-print sess.run(c)
+    print sess.run(c)
+
+with tf.Session() as sess:
+  with tf.device('/gpu:0'):
+    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2,3], name='a')
+    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3,2], name='b')
+    c = tf.matmul(a,b)
+    print sess.run(c)
